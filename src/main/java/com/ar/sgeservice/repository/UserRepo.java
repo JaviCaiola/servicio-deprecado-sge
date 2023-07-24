457f7060d458a96e65b817ad4ddf620e00981539
JaviCaiola <javiteen1250@gmail.com>
2023-07-24 19:47:34 -0300

    creando repositorio del usermodel

--- src/main/java/com/ar/sgeservice/repository/UserRepo.java
+++ src/main/java/com/ar/sgeservice/repository/UserRepo.java
@@ -1,5 +1,11 @@
 package com.ar.sgeservice.repository;
 
-public interface UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ar.sgeservice.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
