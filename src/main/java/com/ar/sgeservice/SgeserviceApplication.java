aa611cf48f8cab06e76e22ef779429f44ade3ec3
JaviCaiola <javiteen1250@gmail.com>
2023-07-24 19:33:50 -0300

    SGE - initlzr

---
+++ src/main/java/com/ar/sgeservice/SgeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ar.sgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeserviceApplication.class, args);
+	}
+
+}
