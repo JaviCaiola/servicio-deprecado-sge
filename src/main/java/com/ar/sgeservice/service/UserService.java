a3cc4c11047df47eb2ed2a9743e3a44d0d964ac5
JaviCaiola <javiteen1250@gmail.com>
2023-07-24 19:53:50 -0300

    Completando la capa service del usuario

--- src/main/java/com/ar/sgeservice/service/UserService.java
+++ src/main/java/com/ar/sgeservice/service/UserService.java
@@ -1,5 +1,14 @@
 package com.ar.sgeservice.service;
 
+import java.util.List;
+
+import com.ar.sgeservice.model.User;
+
 public interface UserService {
 
+	public List<User> allUsers();
+	public User searchUser(Long id);
+	public User newUser (User us);
+	public User updateUser (Long id, User us);
+	public void deleteUser (Long id);
 }
